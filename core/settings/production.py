8969281eb4fd451b5da4e061916f6ad87a3db578
Matt Capelli <matt@inverseapp.com>
2021-02-13 18:13:30 +0300

    Fix missing database variable

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,11 +6,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-# Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = [
     os.environ['HOST_URL'],
