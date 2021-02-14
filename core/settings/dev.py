ffb061e2ac6f6a58e189be9e93a27d155c0d514e
Bui Tan Loc <loc@datasharma.com>
2021-02-15 00:53:14 +0700

    Default dev with zero configuration, but have instructions for custom database, email

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,10 +1,10 @@
+# You could make another 'dev_custom.py' for Database & Email config. Then run with the 'dev_custom' environment like this:
+# ./manage.py runserver --settings=core.settings.dev_custom
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-from .config import *
-DATABASES = local_database
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p6_izlna_zfupnk57int)z2cew$qs7=q(hkm)f^1xhc%5pyurr'
@@ -12,7 +12,26 @@ SECRET_KEY = 'p6_izlna_zfupnk57int)z2cew$qs7=q(hkm)f^1xhc%5pyurr'
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Database Config: provide your own local or remote postgresql
+# DATABASES = {
+#       'default': {
+#           'ENGINE': 'django.db.backends.postgresql',
+#           'NAME': 'one-table-local',
+#           'USER': 'postgres',
+#           'PASSWORD': 'postgres',
+#           'HOST': 'localhost',
+#           'PORT': '5432',
+#       }
+# }
+
+# Sending Email Config: Just use the real smptp server.
+# If you are going to use Gmail. Do not forget to turn on this flag: https://www.google.com/settings/security/lesssecureapps
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'yourGmail@gmail.com'
+# EMAIL_HOST_PASSWORD = 'yourGmailPassword'
+# EMAIL_USE_TLS = True
 
 try:
     from .local import *
