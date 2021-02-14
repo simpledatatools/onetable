fa649e90bb0b504e6596bcb0b4799fdf669b6ae1
Bui Tan Loc <loc@datasharma.com>
2021-02-15 00:54:47 +0700

    Make configurations in order. We will configure: database, email, s3 here, but read from remote params

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,14 +1,16 @@
 from __future__ import absolute_import, unicode_literals
-import os
 from .base import *
 
-SECRET_KEY = os.environ['SECRET_KEY']
+import dj_database_url
+import os
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
 
+# SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = [
     os.environ['HOST_URL'],
     os.environ['ACCESS_URL']
@@ -16,11 +18,15 @@ ALLOWED_HOSTS = [
 
 # Redirect to https in production
 SECURE_SSL_REDIRECT = True
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Database Config: Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config()
 
+# Whitenoise config
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
@@ -28,12 +34,16 @@ COMPRESS_CSS_FILTERS = [
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
+# S3 Config
+INSTALLED_APPS = INSTALLED_APPS + ['storages']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 DEFAULT_FILE_STORAGE = os.environ['DEFAULT_FILE_STORAGE']
 
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_PORT = os.environ['EMAIL_PORT']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
